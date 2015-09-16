390aa4991970e5dcd1df2139a38cf0230fbed45e
Juan Morales <crispamares@gmail.com>
2015-09-16 02:48:37 +0200

    webpack project configured with babel

---
+++ webapp/web/js/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import _ from 'lodash';
+
+// ----------------------------------------------------------
+//  Setup indyva's conection
+// ----------------------------------------------------------
+
+import Context from 'context';
+var context = new Context(window.location.hostname, 'ws', 19000);
+context.install();
+var session = 's'+String(Math.round((Math.random()*100000)));
+context.openSession(session);
+
+window.onbeforeunload = function() {return "The session will be lost";};
+window.onunload = function() {context.closeSession();};
+
+var rpc = context.rpc;
+var hub = context.hub;
+
+React.render( <div> PACOOOOO </div>, document.getElementById('content'));
