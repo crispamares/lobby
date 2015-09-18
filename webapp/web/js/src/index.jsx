af1749c935a2cc5e30c8e5c1870ddf98794afa5f
Juan Morales <crispamares@gmail.com>
2015-09-18 12:32:39 +0200

    whenjs working in node

--- webapp/web/js/src/index.jsx
+++ webapp/web/js/src/index.jsx
@@ -7,17 +7,17 @@ import Loader from './loader';
 //  Setup indyva's conection
 // ----------------------------------------------------------
 
-// import Context from 'context';
-// var context = new Context(window.location.hostname, 'ws', 19000);
-// context.install();
-// var session = 's'+String(Math.round((Math.random()*100000)));
-// context.openSession(session);
-//
-// window.onbeforeunload = function() {return "The session will be lost";};
-// window.onunload = function() {context.closeSession();};
-//
-// var rpc = context.rpc;
-// var hub = context.hub;
+import Context from 'context';
+var context = new Context('localhost', 'ws', 19000);
+context.install();
+var session = 's'+String(Math.round((Math.random()*100000)));
+context.openSession(session);
+
+window.onbeforeunload = function() {return "The session will be lost";};
+window.onunload = function() {context.closeSession();};
+
+var rpc = context.rpc;
+var hub = context.hub;
 
 
 class App extends React.Component {
